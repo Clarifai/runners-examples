d7340134cfefe73dc395af3c4d03c6704c195905
phatvo9 <luv.bansal@clarifai.com>
2025-06-04 17:51:14 +0000

    update flux with param

--- text-to-image/flux-schnell/1/test_model_local.py
+++ text-to-image/flux-schnell/1/test_model_local.py
@@ -8,11 +8,12 @@ from model import TextToImageModel
 model = TextToImageModel()
 model.load_model()
 
-images = model.predict(
-    prompt=["A Ghibli animated orange cat, panicked about a deadline, sits in front of a Banana-brand laptop."]*3, 
-    negative_prompt=["Ugly, cute"]*2, guidance_scale=7)
-print(images)
-images[0].to_pil().save("tmp/flux_schnell.jpg")
-images = model.predict(
+image = model.predict(
+    prompt="A Ghibli animated orange cat, panicked about a deadline, sits in front of a Banana-brand laptop.", 
+    negative_prompt="Ugly, cute", guidance_scale=7)
+print(image)
+image.to_pil().save("tmp/flux_schnell.jpg")
+
+images = model.create(
     prompt=["A Ghibli animated orange cat, panicked about a deadline, sits in front of a Banana-brand laptop."]*3, 
     negative_prompt=["Ugly, cute"]*2, guidance_scale=7)
