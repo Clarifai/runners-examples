9d3b05c297f0edc04498afe8af9de85a2f935498
phatvo9 <phat.vo@clarifai.com>
2026-01-02 10:12:54 +0000

    update test

--- text-to-image/flux-schnell/1/model.py
+++ text-to-image/flux-schnell/1/model.py
@@ -106,17 +106,17 @@ class TextToImageModel(ModelClass):
 
     return [Image.from_pil(image) for image in images]
 
-
   def test(self):
     """ 
     Test cases only executed when running `clarifai model test-locally`
     """
     image = self.predict(
-    prompt="A Ghibli animated orange cat, panicked about a deadline, sits in front of a Banana-brand laptop.", 
-    negative_prompt="Ugly, cute", guidance_scale=7)
+        prompt="A Ghibli animated orange cat, panicked about a deadline, sits in front of a Banana-brand laptop.",
+        negative_prompt="Ugly, cute", guidance_scale=7)
     print(image)
 
-    images = self.create(
-        prompt=["A Ghibli animated orange cat, panicked about a deadline, sits in front of a Banana-brand laptop."]*3, 
-        negative_prompt=["Ugly, cute"]*2, guidance_scale=7)
+    images = self.generate_images(
+        prompt="A Ghibli animated orange cat, panicked about a deadline, sits in front of a Banana-brand laptop.",
+        num_images_per_prompt=2,
+        negative_prompt="Ugly, cute", guidance_scale=7)
     print(images)
