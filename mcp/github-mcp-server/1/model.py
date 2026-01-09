8703d04652dff66e78d76152651be292bd3abc7b
Luv Bansal <luv.bansal@clarifai.com>
2026-01-09 17:29:04 +0530

    GITHUB MCP srever

--- mcp/github-mcp-server/1/model.py
+++ mcp/github-mcp-server/1/model.py
@@ -1,5 +1,5 @@
 from clarifai.runners.models.stdio_mcp_class import StdioMCPModelClass
 
 
-class MyBrowserSearchToolClass(StdioMCPModelClass):
+class GithubMCPServerClass(StdioMCPModelClass):
     pass
